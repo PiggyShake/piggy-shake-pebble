b2628a0400e85c8fa764aa36873f93759c631bf1
Caleb Coffie <CalebCoffie@gmail.com>
2015-08-16 01:32:57 -0700

    Got Configuration working!

--- src/socket.js
+++ src/socket.js
@@ -9,10 +9,10 @@ if (localStorage.getItem("groupID") === null) {
 var reconnectInterval = 1000 * 3; // milliseconds
 var WS;
 var connect = function(){
-    WS = new WebSocket('ws://ws.piggyshake.com:8080');
+    WS = new WebSocket('ws://piggy-ubuntu.cloudapp.net:8080');
     WS.onopen = function() {
         console.log('socket open');
-      WS.send('{"groupID": "' + localStorage.getItem("groupID") + '", "devID": "' + Pebble.getWatchToken() + '", "shake": "false"}');
+      WS.send('{"groupID": "' + localStorage.getItem("groupID") + '", "devID": "' + Pebble.getWatchToken() + '", "username": "' + localStorage.getItem("username") + '", "shake": "false"}');
     };
     WS.onerror = function() {
         console.log('socket error');
@@ -34,13 +34,23 @@ connect();
 // Listen for configuration menu launch
 Pebble.addEventListener('showConfiguration', function(e) {
   // Show config page
-  Pebble.openURL('https://piggyshake.com/config.html');
+  Pebble.openURL('https://piggyshake.com/config.html#username=' + encodeURI(localStorage.getItem("username")) + '&channel=' + encodeURI(localStorage.getItem("groupID")));
+});
+
+// Listen for configuration menu close
+Pebble.addEventListener('webviewclosed', function(e) {
+  // Decode and parse config data as JSON
+  var config_data = JSON.parse(decodeURIComponent(e.response));
+  console.log('Config window returned: ', JSON.stringify(config_data));
+  
+  localStorage.setItem("username", config_data.username);
+  localStorage.setItem("groupID", config_data.channel);
 });
 
 // Listen for when an AppMessage is received
 Pebble.addEventListener('appmessage',
   function(e) {
     console.log('AppMessage received!');
-    WS.send('{"groupID": "' + localStorage.getItem("groupID") + '", "devID": "' + Pebble.getWatchToken() + '", "shake": "true"}');
+    WS.send('{"groupID": "' + localStorage.getItem("groupID") + '", "devID": "' + Pebble.getWatchToken() + '", "username": "' + localStorage.getItem("username") + '", "shake": "true"}');
   }                     
 );
